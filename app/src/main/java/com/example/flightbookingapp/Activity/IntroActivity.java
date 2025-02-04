6b4f493229d04ebe05c59100971b1a0b6b3df384
Harge Zuhaib <hargezuhaib@gmail.com>
2025-02-04 17:34:38 +0530

    Dashboard Created:-
    Dashboard Frontend ✅
    Dashboard Backend ✅

--- app/src/main/java/com/example/flightbookingapp/Activity/IntroActivity.java
+++ app/src/main/java/com/example/flightbookingapp/Activity/IntroActivity.java
@@ -1,18 +1,11 @@
-package com.example.flightbookingapp;
+package com.example.flightbookingapp.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.flightbookingapp.databinding.ActivityIntroBinding;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends BaseActivity {
 
     private ActivityIntroBinding binding;
     @Override
---
+++ app/src/main/java/com/example/flightbookingapp/Model/Location.java
@@ -0,0 +1,34 @@
+package com.example.flightbookingapp.Model;
+
+public class Location {
+    private int Id;
+    private String Name;
+
+
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Location() {
+    }
+
+    @Override
+    public String toString() {
+        return  Name;
+
+    }
+
+}
